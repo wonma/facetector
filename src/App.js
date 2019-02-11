06103ede797e6836d3915ecb93109a7bff6292cb
Wonmi <wonmadesign@gmail.com>
2019-02-11 18:36:49 +0900

    meta image fixed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Rank from './components/Rank/Rank'
 import Particles from 'react-particles-js'
 import MetaTags from 'react-meta-tags';
 import AutoUrl from './components/AutoUrl/AutoUrl'
+import metaImage from './images/favicon.png'
 
 // import Clarifai from 'clarifai'
 import 'tachyons'
@@ -233,9 +234,10 @@ class App extends Component {
           <meta name="description" content="We detect faces from your image and give you score!" />
           <meta property="og:title" content="Facetector" />
           <meta property="og:description" content="We detect faces from your image and give you score." />
-          <meta property="og:image" content="./images/favicon.png" />
-          <meta name="twitter:card" content="We detect faces from your image and give you score!" />
+          <meta property="og:image" content={metaImage} />
           <meta property="og:url" content="https://facetector.herokuapp.com/" />
+          <meta property="og:site_name" content="Facetector by Wonmi" />
+          <meta name="twitter:card" content="We detect faces from your image and give you score!" />
         </MetaTags>
         <Particles className='particles'
           params={particleOptions}
